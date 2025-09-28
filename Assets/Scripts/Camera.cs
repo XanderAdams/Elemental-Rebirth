6605fbe430bf8caa50e0e67a1c1b875c360652f4
CooperCantrell <coopercantrell56@gmail.com>
2025-09-28 00:30:05 -0500

    camera follows player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z-10);
+    }
+}
