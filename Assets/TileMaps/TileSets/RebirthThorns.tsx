eb943c9a473422bb8dd63fac5410a58ad94b42b4
Xander Adams <xanderadams555@gmail.com>
2025-09-27 23:47:07 -0500

    Test map

--- Assets/TileMaps/TileSets/RebirthThorns.tsx
+++ Assets/TileMaps/TileSets/RebirthThorns.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="RebirthThorns" tilewidth="64" tileheight="64" tilecount="4" columns="2">
- <image source="../SpriteSheets/Thorns_Rebirth.png" width="128" height="128"/>
+<tileset version="1.10" tiledversion="1.11.2" name="RebirthThorns" tilewidth="64" tileheight="64" tilecount="9" columns="3">
+ <image source="../SpriteSheets/Thorns_Rebirth.png" width="192" height="192"/>
 </tileset>
