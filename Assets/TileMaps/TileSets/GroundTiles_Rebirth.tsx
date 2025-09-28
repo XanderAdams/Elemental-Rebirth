b20a01e28a9ee982900d25218d0a80e707c803de
Xander Adams <xanderadams555@gmail.com>
2025-09-28 00:05:54 -0500

    Tile Colliders

--- Assets/TileMaps/TileSets/GroundTiles_Rebirth.tsx
+++ Assets/TileMaps/TileSets/GroundTiles_Rebirth.tsx
@@ -1,4 +1,129 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="GroundTiles_Rebirth" tilewidth="64" tileheight="64" tilecount="25" columns="5">
  <image source="../SpriteSheets/GroundTiles_Rebirth.png" width="320" height="320"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="42" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
 </tileset>
--- Assets/TileMaps/TileSets/RebirthThorns.tsx
+++ Assets/TileMaps/TileSets/RebirthThorns.tsx
@@ -1,4 +1,55 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="RebirthThorns" tilewidth="64" tileheight="64" tilecount="9" columns="3">
  <image source="../SpriteSheets/Thorns_Rebirth.png" width="192" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="46" width="64" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0.25" y="0.25">
+    <polygon points="0,0 14.5,-0.25 15.25,47 63.25,47.5 63.25,63.75 0.25,63"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.5" y="16.5">
+    <polygon points="-0.25,-0.75 48.75,-0.75 48.5,-16.25 -15.25,-16.25 -15.5,47 48.25,47.25 49,31.75 0,32.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="64" height="64"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0.75" y="50" width="13.25" height="14.25"/>
+   <object id="5" x="50.75" y="50.75" width="13.25" height="13.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.5" y="-0.5" width="14.5" height="15.75"/>
+   <object id="3" x="0" y="50" width="14" height="13.75"/>
+   <object id="7" x="49" y="49" width="15.25" height="14.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.25" y="-0.25" width="14.75" height="14.25"/>
+   <object id="3" x="49" y="0" width="14.75" height="14.25"/>
+   <object id="5" x="0.25" y="49.75" width="14.25" height="14.5"/>
+   <object id="6" x="49.75" y="50" width="14.25" height="14.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="0" width="14.75" height="15.25"/>
+   <object id="2" x="50" y="49" width="14.75" height="14.75"/>
+  </objectgroup>
+ </tile>
 </tileset>
